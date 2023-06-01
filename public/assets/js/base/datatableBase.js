2dd1f8896d2ac36e2be3bef04041636cc7bfc51c
Diego Vidal Caro <dvidalcaro@hotmail.com>
2023-06-01 02:30:44 -0300

    Se agrega botones de descarga

--- public/assets/js/base/datatableBase.js
+++ public/assets/js/base/datatableBase.js
@@ -1,33 +1,79 @@
 $(document).ready(function () {
-    $('#example').DataTable({
-        language: {
-            url: './assets/js/base/es-AR.json',
-        },
-        dom:'Bfrltip',
-        buttons: [
-            'pdf'
-        ],
-        initComplete: function () {
-            this.api()
-                .columns()
-                .every(function () {
-                    var column = this;
-                    var select = $('<select><option value=""></option></select>')
-                        .appendTo($(column.footer()).empty())
-                        .on('change', function () {
-                            var val = $.fn.dataTable.util.escapeRegex($(this).val());
+  $("#example tfoot th").each(function () {
+    var title = $(this).text();
+    $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+  });
 
-                            column.search(val ? '^' + val + '$' : '', true, false).draw();
-                        });
+  $("#example").DataTable({
+    language: {
+      url: "./assets/js/base/es-AR.json",
+    },
+    dom: "Bfrltip",
+    buttons: [
+      {
+        extend: "pdfHtml5",
+        text: "Exportar a PDF",
+        className: "btn btn-primary",
+        orientation: "landscape",
+        pageSize: "A3",
+      },
+      {
+        extend: "excel",
+        text: "Exportar a Excel",
+        className: "btn btn-success",
+      },
+    ],
 
-                    column
-                        .data()
-                        .unique()
-                        .sort()
-                        .each(function (d, j) {
-                            select.append('<option value="' + d + '">' + d + '</option>');
-                        });
-                });
-        },
-    });
+    initComplete: function () {
+      // Apply the search
+      this.api()
+        .columns()
+        .every(function () {
+          var that = this;
+
+          $("input", this.footer()).on("keyup change clear", function () {
+            if (that.search() !== this.value) {
+              that.search(this.value).draw();
+            }
+          });
+        });
+    },
+    /*  initComplete: function () {
+      // Apply the search
+      this.api()
+        .columns()
+        .every(function () {
+          var that = this;
+          $(".buscador").on("keyup change clear", function () {
+            console.log(this.value);
+            console.log(that.search);
+            if (that.search() !== this.value) {
+              that.search(this.value).draw();
+            }
+          });
+        });
+    }, */
+    /* initComplete: function () {
+      this.api()
+        .columns()
+        .every(function () {
+          var column = this;
+          var select = $('<select><option value=""></option></select>')
+            .appendTo($(column.footer()).empty())
+            .on("change", function () {
+              var val = $.fn.dataTable.util.escapeRegex($(this).val());
+
+              column.search(val ? "^" + val + "$" : "", true, false).draw();
+            });
+
+          column
+            .data()
+            .unique()
+            .sort()
+            .each(function (d, j) {
+              select.append('<option value="' + d + '">' + d + "</option>");
+            });
+        });
+    }, */
+  });
 });
