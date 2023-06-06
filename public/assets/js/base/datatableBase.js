89ed0c6ba2b3bfbfc4a4c4ccaac348c0cbdb0493
Diego Vidal Caro <dvidalcaro@hotmail.com>
2023-06-06 00:49:14 -0300

    Se agrega filtro de busqueda en el thead

--- public/assets/js/base/datatableBase.js
+++ public/assets/js/base/datatableBase.js
@@ -2,40 +2,7 @@ $(document).ready(function () {
   var table = $("#example").DataTable({
     orderCellsTop: true,
     fixedHeader: true,
-    language: {
-      url: "./assets/js/base/es-AR.json",
-    },
-  });
-
-  //Creamos una fila en el head de la tabla y lo clonamos para cada columna
-  $("#example thead tr").clone(true).appendTo("#example thead");
-
-  $("#example thead tr:eq(1) th").each(function (i) {
-    var title = $(this).text(); //es el nombre de la columna
-    $(this).html(
-      '<input type="text" style="width:100%" placeholder="Buscar" />'
-    );
-
-    $("input", this).on("keyup change", function () {
-      if (table.column(i).search() !== this.value) {
-        table.column(i).search(this.value).draw();
-      }
-    });
-  });
-});
-
-/* $(document).ready(function () {
-   
-  $("#example tfoot th").each(function () {
-    var title = $(this).text();
-    $(this).html(
-      '<input type="text" style="width:100%" placeholder="Buscar por ' +
-        title +
-        '" />'
-    );
-  });
-
-  $("#example").DataTable({
+    responsive: true,
     language: {
       url: "./assets/js/base/es-AR.json",
     },
@@ -54,21 +21,21 @@ $(document).ready(function () {
         pageSize: "A3",
       },
     ],
+  });
 
-    initComplete: function () {
-      // Apply the search
-      this.api()
-        .columns()
-        .every(function () {
-          var that = this;
+  //Creamos una fila en el head de la tabla y lo clonamos para cada columna
+  $("#example thead tr").clone(true).appendTo("#example thead");
 
-          $("input", this.footer()).on("keyup change clear", function () {
-            if (that.search() !== this.value) {
-              that.search(this.value).draw();
-            }
-          });
-        });
-    },
+  $("#example thead tr:eq(1) th").each(function (i) {
+    var title = $(this).text(); //es el nombre de la columna
+    $(this).html(
+      '<input style="width:100%" type="text" placeholder="Buscar..." />'
+    );
+
+    $("input", this).on("keyup change", function () {
+      if (table.column(i).search() !== this.value) {
+        table.column(i).search(this.value).draw();
+      }
+    });
   });
 });
- */
